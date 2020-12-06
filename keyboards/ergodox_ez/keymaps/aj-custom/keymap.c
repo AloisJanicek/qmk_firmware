5142793325db66d051c315f83af4ff8eb6bc92e7
Alois Janíček <janicek.dev@gmail.com>
2020-12-06 21:49:03 +0100

    ergodox_ez aj-custom: add new backslash placement

--- keyboards/ergodox_ez/keymaps/aj-custom/keymap.c
+++ keyboards/ergodox_ez/keymaps/aj-custom/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  L2  |           |  L2  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LCtrl |   "  |      |  (   |   )  |                                       |      |  <   |  >   |  '   | RCtrl|
+ *   |LCtrl |   "  |      |  (   |   )  |                                       |   \  |  <   |  >   |  '   | RCtrl|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      | MEH  |       | HYPR |  Esc |
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   OSL(SYMB),    KC_Y,    KC_U,    KC_I,    KC_O,         KC_P,            KC_RBRACKET,
                 KC_H,    KC_J,    KC_K,    KC_L,         KC_SCLN,         OSM(MOD_RALT),
   OSL(MDIA),    KC_N,    KC_M,    KC_COMM, KC_DOT,       KC_SLSH,         OSM(MOD_RSFT),
-                         KC_NO,   KC_LT,   KC_GT,        KC_QUOTE,        OSM(MOD_RCTL),
+                      KC_BSLS,    KC_LT,   KC_GT,        KC_QUOTE,        OSM(MOD_RCTL),
   KC_HYPR, KC_ESC,
   OSM(MOD_RGUI),
   OSM(MOD_LALT), KC_TAB, KC_ENT
